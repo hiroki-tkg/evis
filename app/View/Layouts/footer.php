cb6ec86aed50cb08e7b2e196f0332f42a19123cf
Hiroki Takagi <tkg.japan@gmail.com>
2014-12-06 15:09:26 +0700

    footerの作成

--- app/View/Layouts/footer.php
+++ app/View/Layouts/footer.php
@@ -10,6 +10,39 @@
 
 	</div>
 
+	<div class="row footer_row">
+		<div class="col-md-12 footer_col">
+			<div class="footer">
+				<div class="container">
+					<div class="clearfix">
+						<div class="footer-logo"><a href="#"><img src="img/footer-logo.png" />Evitter</a></div>
+						<dl class="footer-nav">
+							<dt class="nav-title">ABOUT</dt>
+							<dd class="nav-item"><a href="#">About Evitter</a></dd>
+							<dd class="nav-item"><a href="#">Vision</a></dd>
+						</dl>
+						<dl class="footer-nav">
+							<dt class="nav-title">CONTENTS</dt>
+							<dd class="nav-item"><a href="/">つぶやき一覧</a></dd>
+							<dd class="nav-item"><a href="/anonymous">アノニマス一覧</a></dd>
+							<dd class="nav-item"><a href="/albums">アルバム</a></dd>
+						</dl>
+						<dl class="footer-nav">
+							<dt class="nav-title">GALLERY</dt>
+						</dl>
+						<dl class="footer-nav">
+							<dt class="nav-title">CONTACT</dt>
+							<dd class="nav-item"><a href="http://twitter.com/hiroki_tkg/">Twitter</a></dd>
+							<dd class="nav-item"><a href="http://facebook.com/hiroki.takagi/">Facebook</a></dd>
+							<dd class="nav-item"><a href="mailto:hiroki@event-carnival.com">Email</a></dd>
+						</dl>
+					</div>
+					<div class="footer-copyright text-center">Copyright &copy; 2014 <a href="http://twitter.com/hiroki_tkg/">@hiroki_tkg</a> .All rights reserved.</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 	</div>
 
 
